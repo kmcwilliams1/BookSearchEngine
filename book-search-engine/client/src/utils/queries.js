3b2ae5312a106fb6bb68157c9e689689cb2a826c
Kyle McWilliams <kmac52694@gmail.com>
2021-10-13 13:58:36 -0500

    commiting more!

--- book-search-engine/client/src/utils/queries.js
+++ book-search-engine/client/src/utils/queries.js
@@ -18,3 +18,25 @@ export const GET_ME = gql`
     }
   }
 `;
+
+// export const QUERY_MATCHUPS = gql`
+//   query getAllMatchups {
+//     matchups {
+//       _id
+//       tech1
+//       tech2
+//     }
+//   }
+// `;
+
+// export const QUERY_MATCHUP = gql`
+//   query getMatchup($_id: ID!) {
+//     matchup(_id: $_id) {
+//       _id
+//       tech1
+//       tech2
+//       tech1_votes
+//       tech2_votes
+//     }
+//   }
+// `;
---
+++ book-search-engine/server/schemas/typeDefs.js
@@ -0,0 +1,48 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int!
+    password: String!
+    savedBooks: [Book]
+  }
+  
+  input SearchBook {
+    bookId: ID!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String!
+    link: String!
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String!
+    link: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(newBook: SearchBook!): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
